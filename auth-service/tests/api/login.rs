e0cefe794753adb0149da9b9f7e1c92376634494
mskdmage <mskdmage@gmail.com>
2026-02-19 23:51:54 -0500

    [Sprint 1][Task 5] test: Moved individual tests to dedicated files.

---
+++ auth-service/tests/api/login.rs
@@ -0,0 +1,8 @@
+use crate::helpers::TestApp;
+
+#[tokio::test]
+async fn post_login_returns_ok() {
+    let app = TestApp::new().await;
+    let response = app.post_login().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
---
+++ auth-service/tests/api/verify_token.rs
@@ -0,0 +1,8 @@
+use crate::helpers::TestApp;
+
+#[tokio::test]
+async fn post_verify_token_returns_ok() {
+    let app = TestApp::new().await;
+    let response = app.post_verify_token().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
