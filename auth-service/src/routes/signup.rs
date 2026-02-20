3fe114a89facfcb2bf3e1a025b0ef0fc081fda7d
mskdmage <mskdmage@gmail.com>
2026-02-20 01:04:41 -0500

    [Sprint 1][Task 6] feat: Adjusted signup route to implement contract according to spec.

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,8 +1,20 @@
+use serde::Deserialize;
 use axum::{
     response::IntoResponse,
     http::StatusCode,
+    extract::Json,
 };
 
-pub async fn signup_handler() -> impl IntoResponse {
+pub async fn signup_handler(Json(body): Json<SignupRequestBody>) -> impl IntoResponse {
     StatusCode::OK.into_response()
+}
+
+
+// Payload
+#[derive(Deserialize)]
+pub struct SignupRequestBody {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "requires2FA")]
+    pub requires_2fa: bool,
 }
